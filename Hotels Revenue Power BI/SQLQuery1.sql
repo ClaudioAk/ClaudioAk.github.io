10e25c7ac3dfe8b3afbfb2ea7d2c1fb90289990d
ClaudioAk <149435898+ClaudioAk@users.noreply.github.com>
2023-10-31 04:41:10 +0200

    Update SQLQuery1.sql

--- Hotels Revenue Power BI/SQLQuery1.sql
+++ Hotels Revenue Power BI/SQLQuery1.sql
@@ -1,13 +1,13 @@
 
-With Hotels As (
-Select * from dbo.['2018$']
-Union
-Select * from dbo.['2019$']
-Union
-Select * from dbo.['2020$'])
+WITH Hotels AS (
+SELECT * FROM dbo.['2018$']
+UNION
+SELECT * FROM dbo.['2019$']
+UNION
+SELECT * FROM dbo.['2020$'])
 
-Select * From Hotels
-left join dbo.market_segment$
-on Hotels.market_segment = market_segment$.market_segment
-left join dbo.meal_cost$
-on meal_cost$.meal = Hotels.meal
+SELECT * FROM Hotels
+LEFT JOIN dbo.market_segment$
+ON Hotels.market_segment = market_segment$.market_segment
+LEFT JOIN dbo.meal_cost$
+ON meal_cost$.meal = Hotels.meal
